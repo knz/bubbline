5353817f30b0d75ecae4c71b98cc0cf5df516fbf
Raphael 'kena' Poss <knz@thaumogen.net>
2022-08-14 12:29:03 +0200

    Add some autocomplet examples.

--- example/main.go
+++ example/main.go
@@ -32,7 +32,18 @@ Press Ctrl+C to interrupt; Ctrl+D to terminate.`)
 			p = 0
 		}
 		msg = fmt.Sprintf("autocomplete: ...%s", v[p:cursor])
-		return msg, "<TAB>"
+		complete := ""
+		if v[p:cursor] == "lorem" {
+			complete = ` ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`
+		} else if v[p:cursor] == "hello" {
+			complete = " world"
+		} else if strings.ToLower(v[p:cursor]) == "all h" {
+			complete = "uman beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood."
+		} else {
+			msg += "\ntip: try completing after 'lorem'"
+		}
+		return msg, complete
 	}
 
 	m.CheckInputComplete = func(v string, row int) bool {
