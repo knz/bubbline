046c3d5410ea3257fa223d424861abcb0225c70e
Raphael 'kena' Poss <knz@thaumogen.net>
2022-10-07 17:47:56 +0200

    Update to the latest textarea.

--- editline/internal/textarea/refresh.sh
+++ editline/internal/textarea/refresh.sh
@@ -6,6 +6,9 @@
 set -euxo pipefail
 
 git clone --depth=1 https://github.com/charmbracelet/bubbles
-cp bubbles/textarea/textarea.go .
+cp bubbles/textarea/textarea.go textarea.go.orig
+cp textarea.go.orig textarea.go
 patch -p0 <textarea.go.diff
 rm -rf bubbles
+diff -uNr textarea.go.orig textarea.go >textarea.go.diff
+
